5e9cc24e08fcaa147b23d2a44403099696f11cc6
MRafliS71 <mraflis@upi.edu>
2022-11-01 15:17:49 +0700

    LogIn Page
    Tapi masih error log in page nya

--- PasienApplication/app/src/main/java/com/kelompok3/fallhuge/LogInActivity.java
+++ PasienApplication/app/src/main/java/com/kelompok3/fallhuge/LogInActivity.java
@@ -2,17 +2,32 @@ package com.kelompok3.fallhuge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private Button masukButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
         //menghilangkan ActionBar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
+
+        masukButton = findViewById(R.id.masukButton);
+        masukButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(LogInActivity.this, MainPagePatientActivity.class);
+                LogInActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
--- PasienApplication/app/src/main/java/com/kelompok3/fallhuge/SignInPasienActivity.java
+++ PasienApplication/app/src/main/java/com/kelompok3/fallhuge/SignInPasienActivity.java
@@ -2,13 +2,27 @@ package com.kelompok3.fallhuge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInPasienActivity extends AppCompatActivity {
+
+    private Button buatakunButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_pasien);
+
+        buatakunButton = findViewById(R.id.buatakunButton);
+        buatakunButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(SignInPasienActivity.this, MainPagePatientActivity.class);
+                SignInPasienActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
